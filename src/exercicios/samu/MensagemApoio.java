8e2ac9ecc40ecdc7e15b3216a1313bb2eac1e3d0
liviamrs <livia@computacao.ufcg.edu.br>
2021-03-18 10:27:10 -0300

    exercicio samu

---
+++ src/exercicios/samu/MensagemApoio.java
@@ -0,0 +1,19 @@
+package exercicios.samu;
+
+public class MensagemApoio {
+    private String autor;
+    private String texto;
+    private String urlImagem;
+    public MensagemApoio(String autor, String texto) {
+
+   }
+
+    public MensagemApoio(String autor, String texto, String urlImagem) {
+
+   }
+
+    public String toString(){
+    	return "";
+    }
+
+}
---
+++ src/exercicios/samu/Tema.java
@@ -0,0 +1,23 @@
+package exercicios.samu;
+
+public class Tema {
+    private String nome;
+    private String id;
+    private Ajuda[] ajudas;
+    public Tema(String nome, int id) {
+
+   }
+
+    public void cadastrarAjuda(Ajuda ajuda){
+
+    }
+
+    public int contarAjudas(){
+    	return 0;
+    }
+
+    public String toString(){
+    	return "";
+    }
+
+}
