f1975792b10ecd2aa4811c90edb6167208012882
unknown <livia@computacao.ufcg.edu.br>
2022-05-17 17:04:27 -0300

    arrays

---
+++ src/exercicios/javabasico/AcimaMediaModular.java
@@ -0,0 +1,38 @@
+package exercicios.javabasico;
+/**
+ * Dirlididi problema  PoFnkEzvk
+ * Modularizando...
+ */
+import java.util.*;
+public class AcimaMediaModular{
+	public static void main(String[] args){
+		String[] numeros;
+		String entrada;
+		Scanner sc = new Scanner(System.in);
+		int num;
+		entrada = sc.nextLine();
+		numeros = entrada.split(" ");
+		
+		int media = media(numeros);
+		imprimeAcimaMedia(numeros, media);	
+	}
+	
+	private static void imprimeAcimaMedia(String[] numeros, int media){
+		int num;
+		for(int j=0; j<numeros.length; j++) {
+			num = Integer.parseInt(numeros[j]);
+			if(num > media){
+				System.out.print(num + " ");
+			}
+		}	
+	}
+	private static int media(String[] numeros){
+		int soma = 0;
+		int num;
+		for(int j=0; j<numeros.length; j++) {
+			num = Integer.parseInt(numeros[j]);
+			soma+=num;
+		}
+		return soma/numeros.length;
+	}
+}
---
+++ src/exercicios/javabasico/DadoMoji.java
@@ -0,0 +1,22 @@
+package exercicios.javabasico;
+
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Scanner;
+
+import exercicios.clubeSerie.Opiniao;
+
+public class DadoMoji {
+	public static void main(String[] args) {
+		String[] dado = {":)", ":*", ":0", "|o", ";)", ":|"};
+		int op, index;
+		Scanner sc = new Scanner(System.in);
+		Random r = new Random(); 
+		do {
+			index = r.nextInt(5);
+			System.out.println(dado[index]);
+			System.out.println("Girar novamente [0,1]?");
+			op = sc.nextInt();
+		}while(op == 1);	
+	}
+}
